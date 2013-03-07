32ea7d0942f1910326f9be2f54cb87792aed33a5
Miklos Maroti <mmaroti@gmail.com>
2013-03-07 01:11:01 -0600

    this is a mess, but should be done to properly support tail recursion (to allow old futures to be reclaimed and not chained)

--- tasync2.js
+++ tasync2.js
@@ -15,14 +15,14 @@ define(function () {
 
 	// ------- Future -------
 
-	var STATE_NULL = 0;
-	var STATE_LISTEN = 1;
+	var STATE_LISTEN = 0;
+	var STATE_ERROR = 1;
 	var STATE_VALUE = 2;
-	var STATE_ERROR = 3;
+	var STATE_FORWARD = 3;
 
 	var Future = function Future_trace_end () {
-		this.state = STATE_NULL;
-		this.value = null;
+		this.state = STATE_LISTEN;
+		this.value = [];
 
 		this.caller = current;
 		this.position = ++current.children;
@@ -32,41 +32,51 @@ define(function () {
 		console.log("tasync future", this.getPath());
 	};
 
-	Future.prototype.setValue = function (value) {
-		assert(this.state === STATE_NULL || this.state === STATE_LISTEN);
-
-		console.log("tasync setvalue", this.getPath(), value);
+	function setValue (future, value) {
+		assert(future instanceof Future && future.state === STATE_LISTEN);
 
 		if (value instanceof Future) {
-			value.addListener(this);
-		} else if (this.state === STATE_NULL) {
-			this.state = STATE_VALUE;
-			this.value = value;
-		} else if (this.state === STATE_LISTEN) {
-			var i, listeners = this.value;
+			for (;;) {
 
-			this.state = STATE_VALUE;
-			this.value = value;
+			}
+		}
+		while (value instanceof Future) {
+			if (value.state === STATE_LISTEN) {
+				addListener(value, setValue, future);
+				return;
+			} else if (value.state === STATE_ERROR) {
+				setError(future, value.value);
+				return;
+			} else {
+				value = value.value;
+			}
+		}
+
+		console.log("tasync setvalue", getPath(future), value);
+
+		if (future.state === STATE_LISTEN) {
+			var i, listeners = future.value;
+
+			future.state = STATE_VALUE;
+			future.value = value;
 
 			for (i = 0; i < listeners.length; ++i) {
 				listeners[i].setValue(value);
 			}
 		}
-	};
+	}
 
-	Future.prototype.addListener = function (target) {
-		if (this.state === STATE_NULL) {
-			this.state = STATE_LISTEN;
-			this.value = [ target ];
-		} else if (this.state === STATE_LISTEN) {
-			this.value.push(target);
-		} else if (this.state === STATE_VALUE) {
-			target.setValue(this.value);
+	function addListener (future, target) {
+		assert(future instanceof Future && target instanceof Future);
+
+		if (future.state === STATE_LISTEN) {
+			future.value.push(target);
+		} else if (future.state === STATE_VALUE) {
+			setValue(target, future.value);
 		} else {
-			assert(this.value instanceof Error);
-			target.setError(this.value);
+			setError(target, future.value);
 		}
-	};
+	}
 
 	function getSlice (trace) {
 		assert(typeof trace === "string");
@@ -101,8 +111,8 @@ define(function () {
 		return trace;
 	}
 
-	Future.prototype.setError = function (error) {
-		assert(this.state === STATE_NULL || this.state === STATE_LISTEN);
+	function setError (future, error) {
+		assert(this.state === STATE_LISTEN);
 
 		if (!(error instanceof Error) || typeof error.trace !== "string") {
 			error.trace = getTrace(this, error);
@@ -110,10 +120,7 @@ define(function () {
 
 		console.log("tasync seterror", this.getPath(), error.message);
 
-		if (this.state === STATE_NULL) {
-			this.state = STATE_ERROR;
-			this.value = error;
-		} else if (this.state === STATE_LISTEN) {
+		if (this.state === STATE_LISTEN) {
 			var i, listeners = this.value;
 
 			this.state = STATE_ERROR;
@@ -123,25 +130,122 @@ define(function () {
 				listeners[i].setError(error);
 			}
 		}
-	};
+	}
+	;
 
 	var ROOT = Object.create(Future.prototype);
 	ROOT.children = 0;
 
 	var current = ROOT;
 
-	Future.prototype.getPath = function () {
-		var future = this, path = [];
+	function getPath (future) {
+		assert(future instanceof Future);
+
+		var path = [];
 		while (future !== ROOT) {
 			path.push(future.position);
 			future = future.caller;
 		}
 		return path;
-	};
+	}
+
+	// ------- delay -------
+
+	function delay (timeout, value) {
+		if (timeout < 0) {
+			return value;
+		}
+		var future = new Future();
+		setTimeout(function () {
+			future.setValue(value);
+		}, timeout);
+		return future;
+	}
+
+	// ------- invoke -------
+
+	function FutureInvoke (func, that, args, index) {
+		Future.apply(this);
+
+		this.func = func;
+		this.that = that;
+		this.args = args;
+		this.index = index;
+	}
+
+	FutureInvoke.prototype = Object.create(Future.prototype);
+
+	function setArgument_trace_start (future, value) {
+		assert(future instanceof FutureInvoke);
+
+		var args = future.args;
+		args[future.index] = value;
+
+		while (++future.index < args.length) {
+			var arg
+			if (args[future.index] instanceof Future) {
+				if (args[future.index].value === UNRESOLVED) {
+					addListener(args[future.index], setArgument_trace_start, future);
+					return;
+				} else if (args[future.index].value instanceof FutureError) {
+					setValue(args[future.index].value);
+					return;
+				} else {
+					args[future.index] = args[future.index].value;
+				}
+			}
+		}
+
+		assert(current === ROOT);
+		current = future;
+
+		try {
+			value = future.func.apply(future.that, args);
+		} catch (err) {
+			value = new FutureError(future, err);
+		}
+
+		current = ROOT;
+
+		if (value instanceof Future) {
+			if (value.value === UNRESOLVED) {
+				addListener(value, setValue, future);
+				return;
+			} else {
+				value = value.value;
+			}
+		}
+
+		setValue(future, value);
+	}
+
+	function invoke_trace_end (func, args, that) {
+		assert(typeof func === "function" && args instanceof Array);
+
+		var index;
+		for (index = 0; index < args.length; ++index) {
+			var arg = args[index];
+			if (arg instanceof Future) {
+				if (arg.state === STATE_LISTEN) {
+					var future = new FutureInvoke(func, that, args, index);
+					arg.addListener(future);
+					return future;
+				} else if (arg.state === STATE_ERROR) {
+					throw arg.value;
+				} else {
+					args[index] = arg.value;
+				}
+			}
+		}
+
+		return func.apply(that, args);
+	}
 
 	// ------- TASYNC -------
 
 	return {
-		Future: Future
+		Future: Future,
+		delay: delay,
+		invoke: invoke_trace_end
 	};
 });
